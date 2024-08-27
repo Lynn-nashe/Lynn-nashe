4f2b33598cb3b8f6e3d848cf3b9e424a2b793631
Lynn-nashe <aulynvimbai@gmail.com>
2024-08-27 14:01:45 +0200

    Add files via upload

---
+++ mockData.js
@@ -0,0 +1,9 @@
+export const mockData = [
+    { id: '1', category: 'Gym Equipment', clicks: 500, purchases: 50, commission: 200.00, sales: 5000.00 },
+    { id: '2', category: 'Training Clothes', clicks: 300, purchases: 30, commission: 150.00, sales: 3000.00 },
+    { id: '3', category: 'Shoes', clicks: 400, purchases: 40, commission: 180.00, sales: 4000.00 },
+    { id: '4', category: 'General Female Wear', clicks: 600, purchases: 60, commission: 250.00, sales: 6000.00 },
+    { id: '5', category: 'Protein Shakes', clicks: 200, purchases: 20, commission: 100.00, sales: 2000.00 },
+    
+  ];
+  
